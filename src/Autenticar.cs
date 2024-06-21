7aaf6d42f397f257087471b12ba5dcf023972532
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-06-21 14:50:25 -0300

    Added authenticate method, logging the user and selecting the module

---
+++ src/Autenticar.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+namespace sap_automation;
+public partial class Program
+{
+  public void Autenticar()
+  {
+    if(!this.driver.FindElements(By.Id("ZAT01C00001")).Any())
+    {
+    this.driver.FindElement(By.Id("sap-user")).SendKeys(this.configuracoes["USUARIO"]);
+    this.driver.FindElement(By.Id("sap-password")).SendKeys(this.configuracoes["PALAVRA"]);
+    this.driver.FindElement(By.Id("LOGON_BUTTON")).Click();
+    }
+    this.driver.FindElement(By.Id("ZAT01C00001")).Click();
+    System.Threading.Thread.Sleep(this.espera["LONGA"]);
+  }
+}
