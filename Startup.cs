87c70d5978e3059b8a8853adef55c6fa024c3904
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-06-21 15:50:51 -0300

    Checking for the existence of requests in the lock file has been implemented

--- Startup.cs
+++ Startup.cs
@@ -10,12 +10,18 @@ public class Startup
       {
         try
         {
-          program.Pesquisar("0413645431", String.Empty);
-          program.SuperTela();
+          Thread.Sleep(10_000);
+          if(!System.IO.File.Exists(program.PRL_LOCKFILE)) return;
+          var solicitacao = System.IO.File.ReadAllText(program.PRL_LOCKFILE, System.Text.Encoding.UTF8);
+          if(solicitacao.Length == 0 || solicitacao.Length > 50) return;
+          Console.WriteLine($"{DateTime.Now} - Solicitação recebida: {solicitacao}.");
+          program.SuperTela(solicitacao);
         }
-        catch (System.Exception)
+        catch (System.Exception erro)
         {
-          
+          System.IO.File.WriteAllText(program.PRL_LOCKFILE, erro.Message, System.Text.Encoding.UTF8);
+          Console.WriteLine($"{DateTime.Now} - {erro.Message}.");
+          return;
         }
       }
     }
