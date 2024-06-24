4e49fb5402cbfc9cec5deae46abf5953418e7f04
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-06-24 11:35:25 -0300

    Added argument recognition and checks

--- Startup.cs
+++ Startup.cs
@@ -15,7 +15,17 @@ public class Startup
           var solicitacao = System.IO.File.ReadAllText(program.PRL_LOCKFILE, System.Text.Encoding.UTF8);
           if(solicitacao.Length == 0 || solicitacao.Length > 50) return;
           Console.WriteLine($"{DateTime.Now} - Solicitação recebida: {solicitacao}.");
-          program.SuperTela(solicitacao);
+          var argumentos = solicitacao.Split(' ');
+          if(argumentos.Length != 3) throw new IndexOutOfRangeException("A solicitação está malformada! As solicitações devem seguir o formato:\n\n`aplicação` `n. instalação` `n. parceiro`");
+          switch (argumentos[0])
+          {
+            case "fatura":
+            case "debito":
+              program.SuperTela(argumentos[1], argumentos[2]);
+            break;
+            default:
+              throw new InvalidOperationException($"A solicitação {argumentos[0]} é inválida!\n\nAs aplicações aceitas no momento são: `fatura` e `debito` somente.");
+          }
         }
         catch (System.Exception erro)
         {
