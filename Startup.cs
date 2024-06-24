27a90b7870877401afa0e4aeb377d020e781362e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-06-24 11:48:36 -0300

    Added argument checking and converted to the correct type. Implemented 'verification' and 'terminate' methods.

--- Startup.cs
+++ Startup.cs
@@ -17,15 +17,19 @@ public class Startup
           Console.WriteLine($"{DateTime.Now} - Solicitação recebida: {solicitacao}.");
           var argumentos = solicitacao.Split(' ');
           if(argumentos.Length != 3) throw new IndexOutOfRangeException("A solicitação está malformada! As solicitações devem seguir o formato:\n\n`aplicação` `n. instalação` `n. parceiro`");
+          if(!Int64.TryParse(argumentos[1], out Int64 instalacao)) throw new InvalidCastException("A instalação não é um número!");
+          if(!Int64.TryParse(argumentos[2], out Int64 parceiro)) throw new InvalidCastException("O parceiro não é um número!");
+          program.Verificar();
           switch (argumentos[0])
           {
             case "fatura":
             case "debito":
-              program.SuperTela(argumentos[1], argumentos[2]);
+              program.SuperTela(instalacao, parceiro);
             break;
             default:
               throw new InvalidOperationException($"A solicitação {argumentos[0]} é inválida!\n\nAs aplicações aceitas no momento são: `fatura` e `debito` somente.");
           }
+          program.Encerrar();
         }
         catch (System.Exception erro)
         {
